94032d3247c3d5063140a1995e1ba472c2d3222e
omgslinux <omgs01@gmail.com>
2025-08-23 09:22:59 +0200

    Manage files for translation

--- src/Form/InitialType.php
+++ src/Form/InitialType.php
@@ -16,10 +16,9 @@ class InitialType extends AbstractType
     {
         $builder
             ->add('sslMode', ChoiceType::class, [
-                'label' => 'Modo SSL',
                 'choices' => [
                     'Let\'s Encrypt' => 'letsencrypt',
-                    'Certificado personalizado' => 'custom',
+                    'Custom certificate' => 'custom',
                 ],
                 //'expanded' => true,
                 //'multiple' => false,
@@ -30,51 +29,41 @@ class InitialType extends AbstractType
                 ]
             ])
             ->add('httpPort', null, [
-                'label' => 'Puerto HTTP',
                 'required' => true,
             ])
             ->add('httpsPort', null, [
-                'label' => 'Puerto HTTPS',
             ])
             ->add('domainSuffix', TextType::class, [
-                'label' => 'Sufijo de dominio del Vhost',
                 'required' => false,
                 'attr' => ['placeholder' => 'Ej: domain.org'],
             ])
             ->add('logDirFormat', ChoiceType::class, [
-                'label' => 'Formato de la ruta de logs',
                 'choices' => [
                     'name.suffix' => 'server.suffix',
                     'suffix/name' => 'suffix/server',
                 ],
             ])
             ->add('sslCertificate', TextType::class, [
-                'label' => 'Ruta al certificado SSL',
                 'required' => false,
                 'attr' => ['placeholder' => 'Ej: /etc/nginx/ssl/cert.pem'],
             ])
             ->add('sslCertificateKey', TextType::class, [
-                'label' => 'Ruta a la clave del certificado SSL',
                 'required' => false,
                 'attr' => ['placeholder' => 'Ej: /etc/nginx/ssl/key.pem'],
             ])
             ->add('wellKnownDir', TextType::class, [
-                'label' => 'Directorio .well-known',
                 'required' => false,
                 'attr' => ['placeholder' => 'Ej: /var/www/_letsencrypt/.well-known'],
             ])
             ->add('clientMaxBodySize', TextType::class, [
-                'label' => 'Tamaño máximo del cuerpo de la petición',
                 'required' => true,
                 'attr' => ['placeholder' => 'Ej: 512M'],
             ])
             ->add('clientBodyTimeout', TextType::class, [
-                'label' => 'Tiempo de espera del cuerpo de la petición',
                 'required' => true,
                 'attr' => ['placeholder' => 'Ej: 300s'],
             ])
             ->add('fastcgiBuffers', TextType::class, [
-                'label' => 'Buffers de FastCGI',
                 'required' => true,
                 'attr' => ['placeholder' => 'Ej: 64 4K'],
             ])
---
+++ translations/messages.en.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'Edit Defaults' => 'Edit defaults',
+    'Domain related' => 'Domain related',
+    'Http port' => 'HTTP port',
+    'Https port' => 'HTTPS port',
+    'Domain suffix' => 'Domain suffix',
+    'Log dir format' => 'Log dir format',
+    'SSL Mode Configuration' => 'SSL Mode Configuration',
+    'Ssl mode' => 'SSL mode',
+    'Custom certificate' => 'Custom certificate',
+    'Ssl certificate' => 'SSL Certificate',
+    'Ssl certificate key' => 'SSL Certificate private key',
+    'Well known dir' => 'Well-known directory',
+    'Body request' => 'Body request',
+    'Client max body size' => 'Client max body size',
+    'Client body timeout' => 'Client body timeout',
+    'Fastcgi buffers' => 'Fastcgi buffers',
+    'Save' => 'Save',
+
+
+];
